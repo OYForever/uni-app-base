b1bbe22aac2d0b500b60a14b5f76a54b26810e35
Zero <478027478@qq.com>
2022-06-17 17:15:30 +0800

    init

---
+++ config/app.config.js
@@ -0,0 +1,9 @@
+export default {
+  PROVIDER: '',
+  /**
+   * 接口根地址
+   */
+  BASE_URL: 'http://localhost:4000',
+	
+	IMG_URL: 'http://localhost:4000/img'
+}
---
+++ pages/demo/demo.vue
@@ -0,0 +1,104 @@
+
+<template>
+  <view class="content">
+    <view class="uni-padding-wrap uni-common-mt">
+      <view class="uni-textarea uni-common-mt">
+        <textarea :value="res"></textarea>
+      </view>
+      <view class="uni-btn-v uni-common-mt">
+        <button type="primary" @click="sendRequest" :loading="loading">发起请求</button>
+        <button type="default" @click="sendRequest1" :loading="loading">发起请求(async/await)</button>
+        <button type="default" @click="upload">上传图片</button>
+      </view>
+    </view>
+  </view>
+</template>
+
+<script>
+  import { UserService } from '../../services/api.service'
+  import ApiConfig from '../../config/api.config'
+  import { async } from 'q'
+
+  export default {
+    data() {
+      return {
+        loading: false,
+        res: ''
+      }
+    },
+    onLoad(option) {
+      //this.sendRequest();
+      //this.sendRequest1();
+    },
+    methods: {
+      upload: function() {
+        let _this = this
+        uni.chooseImage({
+          count: 9, //默认9
+          sizeType: ['compressed'], //可以指定是原图还是压缩图，默认二者都有
+          sourceType: ['album', 'camera'], //从相册选择
+          success: async function(res) {
+            await _this.$base.methods.imgUpload({ imgList: res.tempFilePaths })
+              .then(res => {
+                let arr = []
+                for (let i = 0; i < res.length; i++) {
+                  arr.push(res[i].qnyimg)
+                }
+                let uploadImgList = _this.uploadImgList
+                _this.uploadImgList = uploadImgList.concat(arr)
+              })
+              .catch(err => {
+                console.log(err)
+              })
+          }
+        })
+      },
+      // 方式一
+      sendRequest: function() {
+        this.loading = true
+        this.$http
+          .get(ApiConfig.APP_BASE_API.USER_INFO_URL)
+          .then(res => {
+            this.loading = false
+            console.log('request success', res)
+            uni.showToast({
+              title: '请求成功',
+              icon: 'success',
+              mask: true
+            })
+            this.res = '请求结果: ' + JSON.stringify(res)
+          })
+          .catch(err => {
+            this.loading = false
+            console.log('request fail', err)
+          })
+      },
+
+      //方式二
+      async sendRequest1() {
+        this.loading = true
+        await UserService.get({ id: 1 })
+          .then(res => {
+            this.loading = false
+            console.log('request success', res)
+            uni.showToast({
+              title: '请求成功',
+              icon: 'success',
+              mask: true
+            })
+            this.res = '请求结果: ' + JSON.stringify(res)
+          })
+          .catch(err => {
+            this.loading = false
+            console.log('request fail', err)
+          })
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+  .content {
+    width: 100%;
+  }
+</style>
